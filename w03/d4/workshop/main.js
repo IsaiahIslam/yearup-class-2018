6ad4e965fd5a38eede622afd58f8b9aa49b919a0
Isaiah Islam <iislam@nyc.yearup.org>
2018-11-08 16:56:23 -0500

    what went wrong!

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,3 +1,11 @@
 //This comes from data.js
-var gifs = window.data.data;
+let gifs = window.data.data;
+console.log(gifs);
 
+function gifGenerator() {
+  let element = document.getElementById('gif');
+  element.src =  gifs[gifGenerator].images.original.url
+  let gifGenerator = Math.floor(Math.random() * gifs.length);
+}
+let button = document.querySelector('button');
+button.addEventListener('click', gifGenerator);
